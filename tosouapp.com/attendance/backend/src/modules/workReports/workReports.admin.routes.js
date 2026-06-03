6d65aff5a82006ad66310cbeb8901c4fa7a6cc50
na na <nana123thanhcong@gmail.com>
2026-06-03 11:42:16 +0900

    fix: resolve correct export module path for work reports

--- tosouapp.com/attendance/backend/src/modules/workReports/workReports.admin.routes.js
+++ tosouapp.com/attendance/backend/src/modules/workReports/workReports.admin.routes.js
@@ -9,7 +9,7 @@ const db = require('../../core/database/mysql');
 const { classifyMonthlyDay } = require('../attendance/attendance.classifier');
 
 const s3Service = require('../../core/services/s3.service');
-const { buildXlsx } = require('./workReports.export');
+const { buildXlsx, buildXlsxBook } = require('../attendance/attendance.export');
 
 const isISODate = (s) => /^\d{4}-\d{2}-\d{2}$/.test(String(s || ''));
 const isYM = (s) => /^\d{4}-\d{2}$/.test(String(s || ''));
