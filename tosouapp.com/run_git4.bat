0280873a948dbf76b62bab10db60f858f25aeedd
na na <nana123thanhcong@gmail.com>
2026-06-04 14:14:32 +0900

    fix: update memo extraction logic to support textarea

---
+++ tosouapp.com/run_git4.bat
@@ -0,0 +1,4 @@
+git status > C:\tosouapp.com\git_status.txt
+git add .
+git commit -m "fix: update memo extraction logic to support textarea instead of input" >> C:\tosouapp.com\git_status.txt
+git push origin main >> C:\tosouapp.com\git_status.txt
