7a6b0017e463e3e7fbbecc142505e661c6211545
na na <nana123thanhcong@gmail.com>
2026-07-03 13:30:37 +0900

    feat: remove Requests tab

---
+++ tosouapp.com/do_push3.ps1
@@ -0,0 +1,4 @@
+Set-Location C:\tosouapp.com
+git add .
+git commit -m "feat: remove 申請 (Requests) tab from employee portal navigation" > push_log3.txt 2>&1
+git push origin main >> push_log3.txt 2>&1
